f7e940e189c3d606aa4411cff2b6b4ae3aa19b8e
numa <numa.schilling@gmail.com>
2024-09-18 22:22:09 +0200

    spinner-demo

--- tea_handler.go
+++ tea_handler.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-
 	"github.com/charmbracelet/ssh"
-	"github.com/charmbracelet/wish/bubbletea"
-	zone "github.com/lrstanley/bubblezone"
 )
 
-// You can wire any Bubble Tea model up to the middleware with a function that
-// handles the incoming ssh.Session. Here we just grab the terminal info and
-// pass it to the new model. You can also return tea.ProgramOptions (such as
-// tea.WithAltScreen) on a session by session basis.
 func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 	// This should never fail, as we are using the activeterm middleware.
 	pty, _, _ := s.Pty()
@@ -29,31 +24,16 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 	// use it to create the styles.
 	// The recommended way to use these styles is to then pass them down to
 	// your Bubble Tea model.
-	renderer := bubbletea.MakeRenderer(s)
-	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10"))
-	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8"))
+	/* 	renderer := bubbletea.MakeRenderer(s)
+	   	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10"))
+	   	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8"))
 
-	// bubble zone
-	zone.NewGlobal()
-
-	items := []list.Item{
-		// an ID field has been added here, however it's not required. You could use
-		// any text field as long as it's unique for the zone.
-		item{id: "item_1", title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{id: "item_2", title: "Nutella", desc: "It's good on toast"},
-		item{id: "item_3", title: "Bitter melon", desc: "It cools you down"},
-		item{id: "item_4", title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{id: "item_5", title: "Eight hours of sleep", desc: "I had this once"},
-		item{id: "item_6", title: "Cats", desc: "Usually"},
-		item{id: "item_7", title: "Plantasia, the album", desc: "My plants love it too"},
-	}
-
-	bg := "light"
-	if renderer.HasDarkBackground() {
-		bg = "dark"
-	}
-
-	m := model{
+	   	bg := "light"
+	   	if renderer.HasDarkBackground() {
+	   		bg = "dark"
+	   	}
+	*/
+	/* 	m := model{
 		term:      pty.Term,
 		profile:   renderer.ColorProfile().Name(),
 		width:     pty.Window.Width,
@@ -62,77 +42,152 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 		list:      list.New(items, list.NewDefaultDelegate(), 80, 10),
 		txtStyle:  txtStyle,
 		quitStyle: quitStyle,
-	}
-	m.list.Title = "Left click on an items title to select it"
+	} */
+
+	m := newModel(defaultTime, pty.Term)
 
 	return m, []tea.ProgramOption{tea.WithAltScreen(), tea.WithMouseCellMotion()}
 }
 
-type item struct {
-	id    string
-	title string
-	desc  string
+/*
+This example assumes an existing understanding of commands and messages. If you
+haven't already read our tutorials on the basics of Bubble Tea and working
+with commands, we recommend reading those first.
+
+Find them at:
+https://github.com/charmbracelet/bubbletea/tree/master/tutorials/commands
+https://github.com/charmbracelet/bubbletea/tree/master/tutorials/basics
+*/
+
+// sessionState is used to track which model is focused
+type sessionState uint
+
+const (
+	defaultTime              = time.Minute
+	timerView   sessionState = iota
+	spinnerView
+)
+
+var (
+	// Available spinners
+	spinners = []spinner.Spinner{
+		spinner.Line,
+		spinner.Dot,
+		spinner.MiniDot,
+		spinner.Jump,
+		spinner.Pulse,
+		spinner.Points,
+		spinner.Globe,
+		spinner.Moon,
+		spinner.Monkey,
+	}
+	modelStyle = lipgloss.NewStyle().
+			Width(15).
+			Height(5).
+			Align(lipgloss.Center, lipgloss.Center).
+			BorderStyle(lipgloss.HiddenBorder())
+	focusedModelStyle = lipgloss.NewStyle().
+				Width(15).
+				Height(5).
+				Align(lipgloss.Center, lipgloss.Center).
+				BorderStyle(lipgloss.NormalBorder()).
+				BorderForeground(lipgloss.Color("69"))
+	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
+	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+)
+
+type mainModel struct {
+	state   sessionState
+	timer   timer.Model
+	spinner spinner.Model
+	index   int
+	term    string
 }
 
-func (i item) Title() string       { return zone.Mark(i.id, i.title) }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return zone.Mark(i.id, i.title) }
-
-// Just a generic tea.Model to demo terminal information of ssh.
-type model struct {
-	term      string
-	profile   string
-	width     int
-	height    int
-	bg        string
-	list      list.Model
-	txtStyle  lipgloss.Style
-	quitStyle lipgloss.Style
+func newModel(timeout time.Duration, term string) mainModel {
+	m := mainModel{state: timerView, term: term}
+	m.timer = timer.New(timeout)
+	m.spinner = spinner.New()
+	return m
 }
 
-func (m model) Init() tea.Cmd {
-	return nil
+func (m mainModel) Init() tea.Cmd {
+	// start the timer and spinner on program start
+	return tea.Batch(m.timer.Init(), m.spinner.Tick)
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.height = msg.Height
-		m.width = msg.Width
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "ctrl+c", "q":
 			return m, tea.Quit
-		}
-
-	case tea.MouseMsg:
-		if msg.Button == tea.MouseButtonWheelUp {
-			m.list.CursorUp()
-			return m, nil
-		}
-
-		if msg.Button == tea.MouseButtonWheelDown {
-			m.list.CursorDown()
-			return m, nil
-		}
-
-		if msg.Action == tea.MouseActionRelease && msg.Button == tea.MouseButtonLeft {
-			for i, listItem := range m.list.VisibleItems() {
-				v, _ := listItem.(item)
-				// Check each item to see if it's in bounds.
-				if zone.Get(v.id).InBounds(msg) {
-					// If so, select it in the list.
-					m.list.Select(i)
-					break
-				}
+		case "tab":
+			if m.state == timerView {
+				m.state = spinnerView
+			} else {
+				m.state = timerView
+			}
+		case "n":
+			if m.state == timerView {
+				m.timer = timer.New(defaultTime)
+				cmds = append(cmds, m.timer.Init())
+			} else {
+				m.Next()
+				m.resetSpinner()
+				cmds = append(cmds, m.spinner.Tick)
 			}
 		}
+		switch m.state {
+		// update whichever model is focused
+		case spinnerView:
+			m.spinner, cmd = m.spinner.Update(msg)
+			cmds = append(cmds, cmd)
+		default:
+			m.timer, cmd = m.timer.Update(msg)
+			cmds = append(cmds, cmd)
+		}
+	case spinner.TickMsg:
+		m.spinner, cmd = m.spinner.Update(msg)
+		cmds = append(cmds, cmd)
+	case timer.TickMsg:
+		m.timer, cmd = m.timer.Update(msg)
+		cmds = append(cmds, cmd)
 	}
-	return m, nil
+	return m, tea.Batch(cmds...)
 }
 
-func (m model) View() string {
-	docStyle := lipgloss.NewStyle().Margin(1, 2)
-	s := fmt.Sprintf("Your term is %s\nYour window size is %dx%d\nBackground: %s\nColor Profile: %s", m.term, m.width, m.height, m.bg, m.profile)
-	return m.txtStyle.Render(s) + zone.Scan(docStyle.Render(m.list.View())) + "\n\n" + m.quitStyle.Render("Press 'q' to quit\n")
+func (m mainModel) View() string {
+	var s string
+	model := m.currentFocusedModel()
+	if m.state == timerView {
+		s += lipgloss.JoinHorizontal(lipgloss.Top, focusedModelStyle.Render(fmt.Sprintf("%4s", m.timer.View())), modelStyle.Render(m.spinner.View()))
+	} else {
+		s += lipgloss.JoinHorizontal(lipgloss.Top, modelStyle.Render(fmt.Sprintf("%4s", m.timer.View())), focusedModelStyle.Render(m.spinner.View()))
+	}
+	s += helpStyle.Render(fmt.Sprintf("\ntab: focus next • n: new %s • q: exit\n", model))
+	return s
+}
+
+func (m mainModel) currentFocusedModel() string {
+	if m.state == timerView {
+		return "timer"
+	}
+	return "spinner"
+}
+
+func (m *mainModel) Next() {
+	if m.index == len(spinners)-1 {
+		m.index = 0
+	} else {
+		m.index++
+	}
+}
+
+func (m *mainModel) resetSpinner() {
+	m.spinner = spinner.New()
+	m.spinner.Style = spinnerStyle
+	m.spinner.Spinner = spinners[m.index]
 }
